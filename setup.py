5f806f62804d3444754568525d2ee4a31e83add6
Kanad Rishiraj <kanad.rishiraj@gmail.com>
2025-02-12 07:42:23 +0000

    Add setup.py for package management

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="selenium-web-automation-utils",
+    version="0.1.0",
+    description="A lightweight Selenium automation utility package.",
+    author="Kanad Rishiraj (@RoamingSaint)",
+    author_email="roamingsaint27@gmail.com",
+    url="https://github.com/roamingsaint/selenium-web-automation-utils",
+    packages=find_packages(),
+    install_requires=[
+        "selenium"
+    ],
+    extras_require={
+        "color": ["colorfulPyPrint"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
