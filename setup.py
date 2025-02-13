ba9aae8ce999f9b00bb8add6e54ba5dc0f1407ea
Kanad Rishiraj <kanad.rishiraj@gmail.com>
2025-02-13 03:55:00 +0000

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenium-web-automation-utils",
-    version="0.1.0",
+    version="0.1.1",
     description="A lightweight Selenium automation utility package.",
     author="Kanad Rishiraj (@RoamingSaint)",
     author_email="roamingsaint27@gmail.com",
